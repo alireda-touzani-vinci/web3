1c1c3015b087c1d146fd5c40f0cc13748c66e797
alireda <alireda.touzani@student.vinci.be>
2025-09-26 16:45:12 +0200

    add: cors config

--- S1/backend/app.js
+++ S1/backend/app.js
@@ -1,22 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var expenseRouter = require('./routes/expenses');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var expenseRouter = require("./routes/expenses");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/expenses', expenseRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/expenses", expenseRouter);
 
 module.exports = app;
