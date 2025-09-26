0b4d8907c6bdeba6d556c5b46bd1f1dbc5525634
alireda <alireda.touzani@student.vinci.be>
2025-09-26 16:59:16 +0200

    add: reset route

--- S1/backend/routes/expenses.js
+++ S1/backend/routes/expenses.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
-const { getAllExpenses, addExpense } = require("../services/expenses");
+const {
+  getAllExpenses,
+  addExpense,
+  resetExpenses,
+} = require("../services/expenses");
 
 router.get("/", (req, res) => {
   const expenses = getAllExpenses();
@@ -15,6 +19,16 @@ router.post("/", (req, res) => {
     res.status(201).json(savedExpense);
   } else {
     res.status(500).json({ error: "Impossible to add the expense" });
+  }
+});
+
+router.post("/reset", (req, res) => {
+  const expenses = resetExpenses();
+
+  if (expenses) {
+    res.status(200).json(expenses);
+  } else {
+    res.status(500).json({ error: "Impossible to reset the expenses" });
   }
 });
 
--- S1/backend/services/expenses.js
+++ S1/backend/services/expenses.js
@@ -2,10 +2,21 @@ const fs = require("fs");
 const path = require("path");
 
 const dataFilePath = path.join(__dirname, "../data/expenses.json");
+const initialDataPath = path.join(__dirname, "../data/expenses.init.json");
 
 getAllExpenses = () => {
   try {
     const data = fs.readFileSync(dataFilePath, "utf-8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Error reading data: ", err);
+    return [];
+  }
+};
+
+getInitialExpenses = () => {
+  try {
+    const data = fs.readFileSync(initialDataPath, "utf-8");
     return JSON.parse(data);
   } catch (err) {
     console.error("Error reading data: ", err);
@@ -24,4 +35,15 @@ addExpense = (expense) => {
   }
 };
 
-module.exports = { getAllExpenses, addExpense };
+resetExpenses = () => {
+  try {
+    const initialExpenses = getInitialExpenses();
+    fs.writeFileSync(dataFilePath, JSON.stringify(initialExpenses, null, 2));
+    return initialExpenses;
+  } catch (err) {
+    console.error("Error writing: ", err);
+    return null;
+  }
+};
+
+module.exports = { getAllExpenses, addExpense, resetExpenses };
