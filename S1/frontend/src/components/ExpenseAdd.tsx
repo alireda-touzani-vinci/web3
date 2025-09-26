7fe7499556091fd842c682d48b1726db2e695370
alireda <alireda.touzani@student.vinci.be>
2025-09-26 17:25:43 +0200

    add: fetch to add an expense

--- S1/frontend/src/components/ExpenseAdd.tsx
+++ S1/frontend/src/components/ExpenseAdd.tsx
@@ -6,7 +6,7 @@ type ExpenseAddProps = {
 
 const ExpenseAdd = ({handleAdd}: ExpenseAddProps) => {
   
-  const onAdd = () => {
+  const onAdd = async () => {
     const expense: Expense = {
       id: Date.now().toString(),
       payer: Math.random() < 0.5 ? "Alice" : "Bob",
@@ -15,7 +15,22 @@ const ExpenseAdd = ({handleAdd}: ExpenseAddProps) => {
       date: Date.now().toString(),
     };
 
-    handleAdd(expense);
+    try {
+      const response = await fetch("http://localhost:3000/expenses", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(expense)
+      });
+
+      if(!response.ok) {
+        throw new Error("Error adding expense"); 
+      }
+
+      const savedExpense = await response.json();
+      handleAdd(savedExpense);
+    } catch (err) {
+      console.error("Error fetching add : ", err);
+    }
   };
 
   return <button onClick={onAdd}>Add</button>;
