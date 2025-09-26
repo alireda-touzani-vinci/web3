3321de3818214d04bf36cc0f0f2e6d942ca8cded
alireda <alireda.touzani@student.vinci.be>
2025-09-26 16:17:19 +0200

    backend config

---
+++ S1/backend/routes/expenses.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+const { getAllExpenses, addExpense } = require("../services/expenses");
+
+router.get("/", (req, res) => {
+  const expenses = getAllExpenses();
+  res.json(expenses);
+});
+
+router.post("/", (req, res) => {
+  const newExpense = req.body;
+  const savedExpense = addExpense(newExpense);
+
+  if (savedExpense) {
+    res.status(201).json(savedExpense);
+  } else {
+    res.status(500).json({ error: "Impossible to add the expense" });
+  }
+  req.accepted();
+});
+
+module.exports = router;
---
+++ S1/backend/services/expenses.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const dataFilePath = path.join(__dirname, "../data/expenses.json");
+
+getAllExpenses = () => {
+  try {
+    const data = fs.readFileSync(dataFilePath, "utf-8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Error reading data: ", err);
+    return [];
+  }
+};
+
+addExpense = (expense) => {
+  try {
+    const expenses = getAllExpenses();
+    expenses.push(expense);
+    fs.writeFileSync(dataFilePath, JSON.stringify(expense, null, 2));
+    return expense;
+  } catch (err) {
+    console.error("Error writing : ", err);
+  }
+};
+
+module.exports = { getAllExpenses, addExpense };
